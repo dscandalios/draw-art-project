ae7403872e0db54a3eb91ae4c1e1a61665e0880e
scandycuz <trevorscandalios@gmail.com>
2017-08-13 15:03:43 -0700

    Sets up submission model

---
+++ drawArtDjango/submission/models.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+class Submission(models.Model):
+    name = models.CharField(max_length=200)
+    info = models.TextField(max_length=200)
+    feelings = models.CharField(max_length=200)
+    email = models.CharField(max_length=150, blank=False, null=False)
+
+    def __str__(self):
+        return '{} | {} | {}'.format(self.name, self.info, self.feelings)
